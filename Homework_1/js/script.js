a247d0b5d82a09e948c9d6f2e9e8d3c2aa918422
AbbosJo'rayev[FE16] <118529401+abbos-jurayev@users.noreply.github.com>
2022-11-23 13:42:07 +0500

    Add files via upload

---
+++ Homework_1/js/script.js
@@ -0,0 +1,12 @@
+let obj = {
+    name: "Abbos",
+    surname: "Jo'rayev",
+    age: 14,
+    frontend: "FE16",
+    education: "Open Web Academy"
+}
+console.log(obj)
+
+let one = prompt('enter the first number')
+let two = prompt('enter second number')
+console.log(Number(one) % Number(two)) 
